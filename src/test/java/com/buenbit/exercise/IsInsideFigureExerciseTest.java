e26d60ee53f83e1fa099f084710b2c8cf68159a8
agustin marraco <adm101195@gmail.com>
2022-11-17 16:31:16 -0300

    adding configuration to launch tests from maven

--- src/test/java/com/buenbit/exercise/IsInsideFigureExerciseTest.java
+++ src/test/java/com/buenbit/exercise/IsInsideFigureExerciseTest.java
@@ -2,9 +2,12 @@ package com.buenbit.exercise;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.buenbit.exercise.SampleMatrix.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class IsInsideFigureExerciseTest {
 
   /**
