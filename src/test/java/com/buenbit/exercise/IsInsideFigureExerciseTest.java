65921c94e42a46150931e767f53ae0a8d976829e
Santiago Garcete <santi101093@gmail.com>
2022-12-13 13:03:19 -0300

    first version of solution with green tests

--- src/test/java/com/buenbit/exercise/IsInsideFigureExerciseTest.java
+++ src/test/java/com/buenbit/exercise/IsInsideFigureExerciseTest.java
@@ -19,6 +19,26 @@ public class IsInsideFigureExerciseTest {
     Assert.assertFalse(isInsideFigureExercise.isInside(0, 0));
   }
 
+  @Test
+  public void isInvalidCoordinateTest() {
+    IsInsideFigureExercise isInsideFigureExercise = new IsInsideFigureExercise(initializeExampleMatrix());
+    Assert.assertFalse(isInsideFigureExercise.isInside(1000, 2000));
+  }
+
+  @Test
+  public void shouldBeOutsideAlwaysIfMatrixDoesNotHaveAFigure() {
+    short[][] matrix = initializeExampleMatrixWithoutFigure();
+    IsInsideFigureExercise isInsideFigureExercise = new IsInsideFigureExercise(matrix);
+
+    Assert.assertFalse(isInsideFigureExercise.isInside(5, 2));
+
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix[0].length; column++) {
+        Assert.assertFalse(isInsideFigureExercise.isInside(row, column));
+      }
+    }
+  }
+
   // Add more tests if you feel like it :)
 
   private short[][] initializeExampleMatrix() {
@@ -33,6 +53,15 @@ public class IsInsideFigureExerciseTest {
         { 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
         { 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
         { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    };
+  }
+
+  private short[][] initializeExampleMatrixWithoutFigure() {
+    return new short[][] {
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     };
   }
